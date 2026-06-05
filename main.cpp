2905db9fb5436b86a8a284d00d114f49deb0522c
Rubi Galindo <a25110251@ceti.mx>
2026-06-05 10:53:33 -0600

    Bienvenida

--- main.cpp
+++ main.cpp
@@ -2,47 +2,49 @@
 #include <SFML/Audio.hpp>
 
 int main() {
-    sf::RenderWindow ventana(sf::VideoMode({800, 600}), "Diseña tu auto de carreras");
+    sf::RenderWindow ventana(sf::VideoMode(800, 600), "Diseña tu auto de carreras");
 
+    // Fondo
     sf::Texture fondoTex;
-    fondoTex.openFromFile("recursos/intro.png");
+    fondoTex.loadFromFile("recursos/intro.png");
     sf::Sprite fondo(fondoTex);
 
+    // Fuente y textos
     sf::Font fuente;
-    fuente.openFromFile("recursos/arial.ttf");
+    fuente.loadFromFile("recursos/arial.ttf");
 
-    sf::Text titulo(fuente, "Diseña tu auto de carreras", 50);
+    sf::Text titulo("Diseña tu auto de carreras", fuente, 50);
     titulo.setFillColor(sf::Color::White);
     titulo.setStyle(sf::Text::Bold);
-    titulo.setPosition({100.f, 150.f});
+    titulo.setPosition(100.f, 150.f);
 
-    sf::Text botonPlay(fuente, "PLAY", 40);
+    sf::Text botonPlay("PLAY", fuente, 40);
     botonPlay.setFillColor(sf::Color::Yellow);
-    botonPlay.setPosition({350.f, 400.f});
+    botonPlay.setPosition(350.f, 400.f);
 
+    // Música de fondo
     sf::Music musica;
     if (musica.openFromFile("recursos/musica_intro.ogg")) {
-        musica.setRepeating(true);
+        musica.setLoop(true);
         musica.play();
     }
 
+    // Sonido burbuja
     sf::SoundBuffer bufferBurbuja;
-    bufferBurbuja.openFromFile("recursos/burbuja.wav");
+    bufferBurbuja.loadFromFile("recursos/burbuja.wav");
     sf::Sound sonidoBurbuja(bufferBurbuja);
 
     bool enIntro = true;
 
     while (ventana.isOpen()) {
-        while (auto evento = ventana.pollEvent()) {
-            if (evento->is<sf::Event::Closed>())
+        sf::Event evento;
+        while (ventana.pollEvent(evento)) {
+            if (evento.type == sf::Event::Closed)
                 ventana.close();
 
-            if (enIntro && evento->is<sf::Event::KeyPressed>()) {
-                auto keyEvent = evento->getIf<sf::Event::KeyPressed>();
-                if (keyEvent && keyEvent->code == sf::Keyboard::Key::Enter) {
-                    sonidoBurbuja.play();
-                    enIntro = false;
-                }
+            if (enIntro && evento.type == sf::Event::KeyPressed && evento.key.code == sf::Keyboard::Enter) {
+                sonidoBurbuja.play();
+                enIntro = false;
             }
         }
 
